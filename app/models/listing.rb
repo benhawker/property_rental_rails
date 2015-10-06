e79d9374746a7737c3346d39ba30b39a53305610
Ben <ben.c.hawker@gmail.com>
2015-10-06 15:28:59 +0100

    Photo and listing associations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,6 @@
 class Listing < ActiveRecord::Base
 	belongs_to :user
+	has_many :photos
 
 	validates :name, length: {minimum: 3}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151006140446) do
+ActiveRecord::Schema.define(version: 20151006141545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,10 @@ ActiveRecord::Schema.define(version: 20151006140446) do
   create_table "photos", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "listing_id"
   end
+
+  add_index "photos", ["listing_id"], name: "index_photos_on_listing_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -57,4 +60,5 @@ ActiveRecord::Schema.define(version: 20151006140446) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "listings", "users"
+  add_foreign_key "photos", "listings"
 end
