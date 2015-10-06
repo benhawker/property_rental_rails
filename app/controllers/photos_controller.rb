595dbeb4514c659dcdc5cdf00c6d59fb821354a6
Ben <ben.c.hawker@gmail.com>
2015-10-06 21:17:25 +0100

    Photos now have listings id's

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,10 +7,21 @@ class PhotosController < ApplicationController
 	def new
 		@listing = Listing.find(params[:listing_id])
 		@photo = Photo.new
-		# @photo = @listing.photo.build(photo_params)
 	end
 
-  def employer_params
+	def create
+		@listing = Listing.find(params[:listing_id])
+		@photo = @listing.photos.build(photo_params)
+			if @photo.save
+				flash[:notice] = "Photo saved successfuly."
+				redirect_to "/"
+			else
+				flash[:alert] = "Sorry but your photo was not saved."
+				render 'new'
+			end
+	end
+
+  def photo_params
     params.require(:photo).permit(:photo)
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,8 @@
 class Photo < ActiveRecord::Base
 	belongs_to :listing
 
-	has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
-  validates_attachment_size :photo, :less_than => 2.megabytes
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_size :image, :less_than => 2.megabytes
 end
+
