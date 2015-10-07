e4723e44781248bb18133b39e4a96ea3ad06e329
Ben <ben.c.hawker@gmail.com>
2015-10-07 08:31:17 +0100

    Additional devise fields completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
 
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  
   root to: "listings#index"
 
   resources :listings do
     resources :photos
   end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
