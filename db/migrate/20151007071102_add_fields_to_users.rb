178187fea3ef7df2d92d2ff8d314c9b2ab0e4b8d
Ben <ben.c.hawker@gmail.com>
2015-10-07 08:20:22 +0100

    Added additional fields to devise user model

---
+++ db/migrate/20151007071102_add_fields_to_users.rb
@@ -0,0 +1,8 @@
+class AddFieldsToUsers < ActiveRecord::Migration
+  def change
+  	add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :phone_country_code, :integer
+    add_column :users, :phone_number, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151006170314) do
+ActiveRecord::Schema.define(version: 20151007071102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,10 @@ ActiveRecord::Schema.define(version: 20151006170314) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "phone_country_code"
+    t.integer  "phone_number"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
